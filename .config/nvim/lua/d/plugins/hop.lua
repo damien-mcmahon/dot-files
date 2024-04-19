5eb1dec8c8a1c0ab669c6040a4aaf54b57835526
Damien McMahon <damien@macoto.co.uk>
2024-04-19 18:41:18 +0100

    more config changes

--- .config/nvim/lua/d/plugins/hop.lua
+++ .config/nvim/lua/d/plugins/hop.lua
@@ -3,9 +3,9 @@ return {
 	branch = "v2", -- optional but strongly recommended
 	config = function()
 		local hop = require("hop")
-		--
 		-- you can configure Hop the way you like here; see :h hop-config
 		hop.setup({ keys = "etovxqpdygfblzhckisuran" })
+
 		local directions = require("hop.hint").HintDirection
 		vim.keymap.set("n", "f", function()
 			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
